05acb009cc77fab6d596f6adfdf6a0763c90688a
syntel1 <syntel1@SYNPSZD4856.syntelorg.com>
2018-05-21 10:50:38 +0530

    "First MAven Project"

---
+++ first-project/src/main/java/org/fedex/t1/MyTestClass.java
@@ -0,0 +1,13 @@
+package org.fedex.t1;
+
+import org.apache.poi.ss.usermodel.Workbook;
+
+public class MyTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is Maven Project");
+	}
+
+}
